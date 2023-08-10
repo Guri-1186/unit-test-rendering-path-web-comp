4a87311f6af5c9bc2281e64cea7cc5d3f0356b5b
Guri Jikia <jikiaguranda@gmail.com>
2023-08-10 16:47:27 +0400

    made some implementation

--- src/email-validator.js
+++ src/email-validator.js
@@ -1,8 +1,40 @@
 const VALID_EMAIL_ENDINGS = ['gmail.com', 'outlook.com', 'yandex.ru']
-// eslint-disable-next-line
+
 export function validate(email) {
   const emailEnding = email.split('@')[1];
-  return VALID_EMAIL_ENDINGS.includes(emailEnding);
-} 
+   return VALID_EMAIL_ENDINGS.includes(emailEnding);
+  
+}
+
+
+export function validateAsync(email) {
+  return new Promise((resolve, reject) => {
+    const emailEnding = email.split('@')[1];
+    if (VALID_EMAIL_ENDINGS.includes(emailEnding)) {
+      resolve(true);
+    } else {
+      reject(false);
+    }
+  });
+}
+
+
+export function validateWithThrow(email){
+  const emailEnding = email.split('@')[1];
+  if (VALID_EMAIL_ENDINGS.includes(emailEnding)) {
+    return true;
+  } else {
+    throw new Error ('provided email is invalid.');
+  }
+};
+
+
+export function validateWithLog(email) {
+ const isValid = validate(email);
+ console.log('Result:', isValid);
+  return isValid;
+};
+
+
 
 
--- src/tests/emailValidator.test.js
+++ src/tests/emailValidator.test.js
@@ -1,7 +1,33 @@
 import { expect } from 'chai'; 
+import { validate } from '../email-validator.js';
  
 describe('first test', () => { 
   it('should return 2', () => {
     expect(2).to.equal(2);
   }) 
 }); 
+
+
+
+describe('validate - validity of an email address based on its domain', () => {
+    it('should be invalid when the email ending is not included in the VALID_EMAIL_ENDINGS', () => {
+      const invalidEmail = 'invalid@example.com';
+      expect(validate(invalidEmail)).to.be.false;
+    });
+  
+    it('should be invalid when the email ending is empty', () => {
+      const invalidEmail = '';
+      expect(validate(invalidEmail)).to.be.false;
+    });
+  
+    it('should be invalid when email has no domain', () => {
+      const invalidEmail = 'user@';
+      expect(validate(invalidEmail)).to.be.false;
+    });
+  
+    it('should be valid when the email ending is included in the VALID_EMAIL_ENDINGS', () => {
+      const validEmail = 'user@gmail.com';
+      expect(validate(validEmail)).to.be.true;
+    });
+  });
+  
